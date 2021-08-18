18210ca081e4aa715f0f8ba68e127e0798dd17d3
IagoOliveira05 <83736163+IagoOliveira05@users.noreply.github.com>
2021-08-18 15:57:30 -0300

    Add files via upload

---
+++ tkinter-inicial/002_teste.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from selenium import webdriver
+
+
+class Tela:
+    def __init__(self):
+        self.root = root
+        self.configura_tela()
+        self.widgets()
+        root.mainloop()
+
+    def configura_tela(self):
+        self.root.configure(bg='#2A2A72')
+        self.root.geometry('375x667+600+15')        # dimensão, posição em Y, distância do topo da tela
+        
+
+    def widgets(self):
+        ola = Label(
+            self.root,
+            text='MEU INSTAGRAM!',
+            font=('times', 30, 'bold'),
+            bg='#2A2A72',
+            fg='#FFA400'
+            )
+        ola.place(x=50, y=100, height=50, width=300)      #.pack()→ Centraliza no meio    .grid()→ Divide a tela em coluna e linhas     .place()→ Você passa as coordenas
+
+        self.btn_confirma = Button(
+            self.root,
+            text='INSTAGRAM',
+            bg='purple',
+            command=self.clica
+        )
+        self.btn_confirma.place(x=130, y=150, height=30, width=150)
+
+        self.btn_fecha = Button(
+            self.root,
+            text='X',
+            bg='red',
+            command=self.fechar
+        )
+        self.btn_fecha.place(x=10, y=10, height=30, width=30)
+
+    def clica(self):
+        driver = webdriver.Chrome()
+        driver.get('https://www.youtube.com/')
+    
+    def fechar(self):
+        self.root.destroy()
+
+root = Tk()
+Tela()
